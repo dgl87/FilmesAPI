58a84ca3dae7bb7200ff02895f5778b667ae3a3e
Douglas <dglandre@gmail.com>
2022-06-04 13:56:32 -0300

    API with connection dataBase MySQL

---
+++ FilmesAPI/Data/Dtos/ReadFilmeDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class ReadFilmeDto
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O preenchimento do campo Titulo é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O Preenchimento do campo Diretor é obrigatório")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O Genero não pode conter mais de 30 caracteres")]
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A Duração deve estar entre 1 e 600")]
+        public int Duracao { get; set; }
+        public DateTime HoraDaConsulta { get; set; }
+    }
+}
---
+++ FilmesAPI/Data/Dtos/UpdateFilmeDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class UpdateFilmeDto
+    {
+        [Required(ErrorMessage = "O preenchimento do campo Titulo é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O Preenchimento do campo Diretor é obrigatório")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O Genero não pode conter mais de 30 caracteres")]
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A Duração deve estar entre 1 e 600")]
+        public int Duracao { get; set; }
+    }
+}
