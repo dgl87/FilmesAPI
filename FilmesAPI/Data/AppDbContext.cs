46ad519aac1103426689ce5ee60dd0d835d7f2ef
Douglas André <dpaiva@direct.one>
2022-09-08 23:21:53 -0300

    Relacionando Filme e Cinema

--- FilmesAPI/Data/AppDbContext.cs
+++ FilmesAPI/Data/AppDbContext.cs
@@ -21,12 +21,22 @@ namespace FilmesAPI.Data
                 .HasOne(cinema => cinema.Gerente)
                 .WithMany(gerente => gerente.Cinemas)
                 .HasForeignKey(cinema => cinema.GerenteId); //.IsRequired(false); //permite que um cinema exista sem obrigação de ter uma chave de gerente, podendo ser null
-                //.OnDelete(DeleteBehavior.Restrict); //Retiramos deleçã em casacata
+                                                            //.OnDelete(DeleteBehavior.Restrict); //Retiramos deleção em casacata
+            builder.Entity<Sessao>()
+                .HasOne(sessao => sessao.Filme)
+                .WithMany(filme => filme.Sessoes)
+                .HasForeignKey(sessao => sessao.FilmeId);
+
+            builder.Entity<Sessao>()
+                .HasOne(sessao => sessao.Cinema)
+                .WithMany(cinema => cinema.Sessoes)
+                .HasForeignKey(sessao => sessao.CinemaId);
         }
 
         public DbSet<Filme> Filmes { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Endereco> Enderecos { get; set; }
         public DbSet<Gerente> Gerentes { get; set; }
+        public DbSet<Sessao> Sessoes { get; set; }
     }
 }
--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace FilmesAPI.Models
 {
@@ -15,5 +17,7 @@ namespace FilmesAPI.Models
         public string Genero { get; set; }
         [Range(1, 600, ErrorMessage = "A Duração deve estar entre 1 e 600")]
         public int Duracao { get; set; }
+        [JsonIgnore]
+        public virtual List<Sessao> Sessoes { get; set; } 
     }
 }
